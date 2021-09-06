fd3ed620ecada48bb54022eb12b6eaafc68e4823
TYRA EKSTRAND <ekstrandtyra@gmail.com>
2021-09-06 09:08:57 +0200

    first commit

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
