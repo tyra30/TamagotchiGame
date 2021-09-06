bab0c602f1c4374454f06cfd8b7ad58c2d7dd8a3
TYRA EKSTRAND <ekstrandtyra@gmail.com>
2021-09-06 09:36:26 +0200

    class created, class started

---
+++ Tamagotchi/tamagotchi.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tamagotchi
+{
+    public class tamagotchi
+    {
+        private int hunger;
+
+        private int boredom;
+
+        private List<string> words = new List<string>();
+
+        private bool isAlive;
+
+        private Random generator = new Random();
+
+        public string name;
+
+        public void Feed(){
+
+        }
+
+        public void Hi(){
+
+        }
+
+        public void Teach(string word){
+
+        }
+
+        public void Tick(){
+
+        }
+
+        public void PrintStats(){
+
+        }
+
+        public bool GetAlive(){
+            return true;
+        }
+
+        private void ReduceBoredom(){
+            
+        }
+
+
+    }
+}
