fb8059a57b7722a0a40c1a78f80f9830c480469f
TYRA EKSTRAND <ekstrandtyra@gmail.com>
2021-09-15 14:17:05 +0200

    add ChooseName

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -6,7 +6,33 @@ namespace Tamagotchi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           tamagotchi avatar = new tamagotchi();
+
+           avatar.name = ChooseName();
+           
+            Console.ReadLine();
+
+
+
+        }
+
+        static string ChooseName()
+        {
+        string name = "";
+        Console.WriteLine("Hello, congrats on becoming a tamagotchi parent, what would you like to call your little friend?");
+
+        name = Console.ReadLine();
+
+        Console.WriteLine($"What a cute name, take good care of {name}");
+
+        return name;
+        }
+
+        static string Choices(tamagotchi tc)
+        {
+            tc.PrintStats();
+
+            Console.WriteLine("")
         }
     }
 }
