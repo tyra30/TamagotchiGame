6a9b13dc9a98bd448263cdb8683728e4812e802b
TYRA EKSTRAND <ekstrandtyra@gmail.com>
2021-09-15 14:57:53 +0200

    working on choices

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -9,7 +9,7 @@ namespace Tamagotchi
            tamagotchi avatar = new tamagotchi();
 
            avatar.name = ChooseName();
-           
+           Choices(avatar);
             Console.ReadLine();
 
 
@@ -27,12 +27,35 @@ namespace Tamagotchi
 
         return name;
         }
+        //Choose name for tamagotchi
 
-        static string Choices(tamagotchi tc)
+        static void Choices(tamagotchi tc)
         {
+            string answer;
             tc.PrintStats();
+           
 
-            Console.WriteLine("")
+            Console.WriteLine("Taking care of a tamagotchi is hard work, what would you like to do know, teach, feed, greet, or simply nothing");
+
+            answer = Console.ReadLine();
+
+            if (answer == "teach" || answer == "Teach")
+            {
+                tc.Teach(Console.ReadLine());
+            }
+            if (answer == "feed" || answer == "Feed")
+            {
+                tc.Feed();
+            }
+            if (answer == "greet" || answer == "Greet")
+            {
+                tc.Hi();
+            }
+            if (answer == "nothing" || answer == "Nothing" || answer == "simply nothing")
+            {
+                Console.WriteLine("alrighty, poor tamagotchi, arent you supposed to take care of you pet?");
+            }
         }
+        //What do you want to do for you tamagotchi
     }
 }
